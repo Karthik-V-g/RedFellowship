f14d4848b2982358cc030d83ebef6a14d3aea002
karthik <karthikkarthik14112000@gmail.com>
2022-07-27 18:25:11 +0530

    finished login for blood requester

--- app/src/main/java/com/example/redfellowship/SplashScreen.java
+++ app/src/main/java/com/example/redfellowship/SplashScreen.java
@@ -29,7 +29,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent i=new Intent(SplashScreen.this,MainActivity.class);
+                Intent i=new Intent(SplashScreen.this,OneToOneMap.class);
                 startActivity(i);
                 finish();
             }
